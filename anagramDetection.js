abd8d4048c22350993bcd2d367a6194b8afa2796
burd5 <austinburdette5@gmail.com>
2022-06-27 15:31:20 -0600

    completed challenge using toLowercase,split,sort, and join methods

---
+++ anagramDetection.js
@@ -0,0 +1,24 @@
+/*
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+
+*/
+
+var isAnagram = function(test, original) {
+    let sortedTest = test.toLowerCase().split("").sort().join("");
+    let sortedOriginal = original.toLowerCase().split("").sort().join("");
+    if(sortedTest == sortedOriginal){
+      return true;
+    }
+    else{
+      return false;
+    }
+  };
