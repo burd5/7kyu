3ee68e833997204c3ceaba561834efa05a453a46
burd5 <austinburdette5@gmail.com>
2022-09-03 17:51:57 -0600

    completed string challenge

---
+++ multiplyStringsInArray.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+arrMultiply(['9','6']) should return '54'
+*/
+
+function arrMultiply(arr){
+    return String(arr[0] * arr[1]);
+ }
