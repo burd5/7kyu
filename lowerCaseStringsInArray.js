d24b4eb8491d8af396e3a14edcb9a767595481b1
burd5 <austinburdette5@gmail.com>
2022-09-06 19:45:21 -0600

    completed challenge using map() and ternary operator

---
+++ lowerCaseStringsInArray.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Create a function to lowercase all strings in an array. Non-string items should remain unchanged.
+
+Example
+arrayLowerCase(['Red', 'Green']) == ['red', 'green']
+arrayLowerCase([1, 'Green']) == [1, 'green']
+*/
+
+function arrayLowerCase(arr) {
+    return arr.map(string => typeof string == 'string'? string.toLowerCase() : string);
+  }
