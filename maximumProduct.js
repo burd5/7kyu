3b3d9d9a48282ca4056818508a1ce14e7a958fcb
burd5 <austinburdette5@gmail.com>
2022-09-28 08:17:21 -0600

    completed challenge using for loop

---
+++ maximumProduct.js
@@ -0,0 +1,36 @@
+/*
+Task
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Notes
+Array/list size is at least 2.
+
+Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+Input >> Output Examples
+adjacentElementsProduct([1, 2, 3]); ==> return 6
+Explanation:
+The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+*/
+
+function adjacentElementsProduct(array) {
+    let maxProduct = -Infinity;
+    for(let i = 0; i < array.length; i++){
+      let product = array[i] * array[i + 1]
+      if(product > maxProduct){
+        maxProduct = product;
+      }
+    }
+  return maxProduct;
+}
+
+//other solution
+
+function adjacentElementsProduct(array) {
+    let newArr = []
+    for(i=0; i < array.length-1; i++){
+      newArr.push(array[i]*array[i+1])
+    }  
+    return Math.max(...newArr)
+  }
+  
