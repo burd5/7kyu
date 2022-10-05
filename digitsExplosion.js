e499cf5aa79a5053d89db1ad5ecbb147f5fbe5ed
burd5 <austinburdette5@gmail.com>
2022-10-05 06:52:48 -0600

    completed challenge using map and repeat methods

---
+++ digitsExplosion.js
@@ -0,0 +1,13 @@
+/*
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+explode("312")
+should return :
+
+"333122"
+*/
+
+function explode(s) {
+    return s.split('').map( (number) => number.repeat(number)).join('');
+  }
